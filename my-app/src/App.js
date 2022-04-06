b40ee76e4fb4adec706baaa70203997614e8b8e5
Yeonathan Aizenberg <afula.aizenberg@gmail.com>
2022-04-06 20:04:21 +0300

    Background img

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,7 +8,7 @@ import './App.css';
 function App() {
 
   return (
-    <div className="App">
+    <div className="App" style={{backgroundImage: `url(weatherImage.jpg)` }}>
       <Router>
         <NavBar />
         <Routes>
